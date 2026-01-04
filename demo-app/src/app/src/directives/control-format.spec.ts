3f392ad76e1de0a2c4ab97cd7213120644e60e16
truonglv4 <anhtruonglavm2@gmail.com>
2026-01-04 17:23:21 +0700

    chore: remove unused components, styles, and tests related to event binding, ngFor, ngIf, property binding, and two-way binding

--- demo-app/src/app/src/directives/control-format.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ControlFormat } from './control-format';
-
-describe('ControlFormat', () => {
-  it('should create an instance', () => {
-    const directive = new ControlFormat();
-    expect(directive).toBeTruthy();
-  });
-});
